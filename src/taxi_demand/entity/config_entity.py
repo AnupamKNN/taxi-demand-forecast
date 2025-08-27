254ad3e7699b4237e5531e669ed8622022284aba
Anupam Singh <anupam848484@gmail.com>
2025-08-27 15:11:21 +0530

    Data Transformation and Model Trainer pipelines implemented

--- src/taxi_demand/entity/config_entity.py
+++ src/taxi_demand/entity/config_entity.py
@@ -89,8 +89,8 @@ class DataTransformationConfig:
     def __init__(self, training_pipeline_config=TrainingPipelineConfig()):
         self.data_transformation_dir = os.path.join(training_pipeline_config.artifact_dir, training_pipeline.DATA_TRANSFORMATION_DIR_NAME)
         self.data_transformation_transformed_dir = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DIR)
-        self.transformed_train_file_path = os.path.join(self.data_transformation_dir, training_pipeline.TRAIN_FILE_NAME)
-        self.transformed_test_file_path = os.path.join(self.data_transformation_dir, training_pipeline.TEST_FILE_NAME)
+        self.transformed_train_file_path = os.path.join(self.data_transformation_transformed_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.transformed_test_file_path = os.path.join(self.data_transformation_transformed_dir, training_pipeline.TEST_FILE_NAME)
         self.transformed_object_file_path = os.path.join(self.data_transformation_dir, training_pipeline.PREPROCESSOR_OBJECT_FILE_NAME)
 
 
